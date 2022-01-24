a9cdf7801adef90068838e97ea7f2170ab5cfcf1
SergeiTsybulya <sergtsybulya@gmail.com>
2022-01-24 23:30:56 +0200

    Initial Commit

---
+++ Tipsy/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Tipsy
+//
+//  Created by Сергей on 24.01.2022.
+//  Copyright © 2022 STDevelopment. All rights reserved.
+//
+
+import Foundation
---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Сергей on 24.01.2022.
+//  Copyright © 2022 STDevelopment. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
