5ec6e925f740411f327c153fa7d3218ca71344fe
SergeiTsybulya <sergtsybulya@gmail.com>
2022-01-26 23:23:41 +0200

    add stepper

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -26,8 +26,16 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = Int(sender.value).description
     }
     @IBAction func calculatePressed(_ sender: UIButton) {
+        if zeroPctButton.isSelected{
+            print(0)
+        } else if tenPctButton.isSelected{
+            print(0.1)
+        } else {
+            print (0.2)
+        }
     }
     
     
